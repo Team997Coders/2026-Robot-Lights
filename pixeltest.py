9b94f804347b93570cd449e29dbee62d91cea280
Philipp-BH <157858376+Philipp-BH@users.noreply.github.com>
2026-02-18 22:53:27 -0800

    hopefully implemented global alliance and brightness variables

--- pixeltest.py
+++ pixeltest.py
@@ -4,12 +4,21 @@ import utime
 import math
 
 STRIP_LENGTH = 45
+BRIGHTNESS = 1 #adjust for brightness of the LEDs, scale 0-1
+ALLIANCE = "red" #change to "blue" for blue alliance
 
 pin_left = Pin(0)
 pixel_left = neopixel.NeoPixel(pin_left, STRIP_LENGTH) #strips from last year are 45 lights long
 
+def set_pixel(strip, pixel, color):
+    strip[pixel] = (int(color[0] * BRIGHTNESS), int(color[1] * BRIGHTNESS), int(color[2] * BRIGHTNESS))
+def fill_strip(strip, color):
+    strip.fill((int(color[0] * BRIGHTNESS), int(color[1] * BRIGHTNESS), int(color[2] * BRIGHTNESS)))
 
-def idle(alliance):
+
+def idle():
+    global ALLIANCE
+    
     tick = int(round(utime.ticks_ms() / 100))
     
     for i in range (0, STRIP_LENGTH, 1):
@@ -21,14 +30,16 @@ def idle(alliance):
         
 #         print(value)
         
-        if alliance == "red":
-            pixel_left[i] = (value, 0, 0)
-        elif alliance == "blue":
-            pixel_left[i] = (0, 0, value)
+        if ALLIANCE == "red":
+            set_pixel(pixel_left, i, (value, 0, 0))
+        elif ALLIANCE == "blue":
+            set_pixel(pixel_left, i, (0, 0, value))
         else:
             print("invalid alliance color")
 
-def active(alliance):
+def active():
+    global ALLIANCE
+    
     tick = int(round(utime.ticks_ms() / 20))
     
     for i in range (0, STRIP_LENGTH, 1):
@@ -40,10 +51,10 @@ def active(alliance):
         
 #         print(value)
                 
-        if alliance == "red":
-            pixel_left[i] = (value, 0, 0)
-        elif alliance == "blue":
-            pixel_left[i] = (0, 0, value)
+        if ALLIANCE == "red":
+            set_pixel(pixel_left, i, (value, 0, 0))
+        elif ALLIANCE == "blue":
+            set_pixel(pixel_left, i, (0, 0, value))
         else:
             print("invalid alliance color")
 
@@ -55,17 +66,21 @@ def shoot():
         value = value * 0.9
         value = value ** 3
         value = int(round(value))
-        pixel_left[i] = (value, int(round(value / 10)), 0)
+        set_pixel(pixel_left, i, (value, int(round(value / 10)), 0))
         
 #         print(value)
-        
+
 def target_locked():
     tick = int(round(utime.ticks_ms() / 60))
+    
     value = 1 + math.sin(tick)
     value = value * 100
     value = int(round(value))
-    pixel_left.fill((0, value, 0))
-            
+    
+    fill_strip(pixel_left, (0, value, 0))
+  
+          
+          
 pixel_left.fill((0, 0, 0))
 pixel_left.write()
 
@@ -75,5 +90,5 @@ utime.sleep(1)
 while True:
     shoot()
     
-    utime.sleep_ms(20)
+    utime.sleep_ms(20) #adjust for peak frames per second of the robot, 20ms is approx 50fps max rendering ability
     pixel_left.write()
