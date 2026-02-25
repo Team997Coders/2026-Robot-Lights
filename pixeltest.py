72d0d6c15f285ab30c0b783784a9b5de608095b1
Philipp-BH <157858376+Philipp-BH@users.noreply.github.com>
2026-02-24 17:55:29 -0800

    started main program based on test file, added means of reading status from rio

--- pixeltest.py
+++ pixeltest.py
@@ -7,8 +7,18 @@ STRIP_LENGTH = 45
 BRIGHTNESS = 1 #adjust for brightness of the LEDs, scale 0-1
 ALLIANCE = "blue" #change to "blue" for blue alliance
 
-pin_left = Pin(0)
-pixel_left = neopixel.NeoPixel(pin_left, STRIP_LENGTH) #strips from last year are 45 lights long
+STATUS_LIST = ["idle", "active", "shoot", "target_search", "target_locked", "climbing", "climbed", "alert"]
+
+in0 = Pin(2, Pin.IN, Pin.PULL_DOWN)
+in1 = Pin(3, Pin.IN, Pin.PULL_DOWN)
+in2 = Pin(4, Pin.IN, Pin.PULL_DOWN)
+
+dual_strip_pin = Pin(0)
+dual_strip = neopixel.NeoPixel(dual_strip_pin, STRIP_LENGTH) #strips from last year are 45 lights long
+
+def read_status():
+    status = in0.value() + (in1.value() * 2) + (in2.value() * 4)
+    return status
 
 def set_pixel(strip, pixel, color):
     strip[pixel] = (int(color[0] * BRIGHTNESS), int(color[1] * BRIGHTNESS), int(color[2] * BRIGHTNESS))
@@ -31,9 +41,9 @@ def idle():
 #         print(value)
         
         if ALLIANCE == "red":
-            set_pixel(pixel_left, i, (value, 0, 0))
+            set_pixel(dual_strip, i, (value, 0, 0))
         elif ALLIANCE == "blue":
-            set_pixel(pixel_left, i, (0, 0, value))
+            set_pixel(dual_strip, i, (0, 0, value))
         else:
             print("invalid alliance color")
 
@@ -52,9 +62,9 @@ def active():
 #         print(value)
                 
         if ALLIANCE == "red":
-            set_pixel(pixel_left, i, (value, 0, 0))
+            set_pixel(dual_strip, i, (value, 0, 0))
         elif ALLIANCE == "blue":
-            set_pixel(pixel_left, i, (0, 0, value))
+            set_pixel(dual_strip, i, (0, 0, value))
         else:
             print("invalid alliance color")
 
@@ -66,7 +76,7 @@ def shoot():
         value = value * 0.9
         value = value ** 3
         value = int(round(value))
-        set_pixel(pixel_left, i, (value, int(round(value / 10)), 0))
+        set_pixel(dual_strip, i, (value, int(round(value / 10)), 0))
         
 #         print(value)
 
@@ -75,16 +85,9 @@ def target_search():
     
     for i in range (0, STRIP_LENGTH, 1):
         if (tick + i) % 3 == 0:
-            set_pixel(pixel_left, i, (0, 255, 0))
+            set_pixel(dual_strip, i, (0, 255, 0))
         else:
-            set_pixel(pixel_left, i, (0, 20, 0))
-    
-#     for i in range (0, STRIP_LENGTH, 1):
-#         value = ((i + 50) - tick) % 8
-#         value = value * 0.9
-#         value = value ** 3
-#         value = int(round(value))
-#         set_pixel(pixel_left, i, (0, value, 0))
+            set_pixel(dual_strip, i, (0, 20, 0))
 
 def target_locked():
     tick = int(round(utime.ticks_ms() / 20))
@@ -93,7 +96,7 @@ def target_locked():
     value = value * 100
     value = int(round(value))
     
-    fill_strip(pixel_left, (0, value, 0))
+    fill_strip(dual_strip, (0, value, 0))
     
 def climbing():
     tick = int(round(utime.ticks_ms() / 10))
@@ -102,15 +105,15 @@ def climbing():
     segment_length = STRIP_LENGTH * 1.25
     
     if cycletick < STRIP_LENGTH:
-        set_pixel(pixel_left, int(tick % segment_length), (255, 0, 255))
+        set_pixel(dual_strip, int(tick % segment_length), (255, 0, 255))
     elif cycletick > STRIP_LENGTH * 1.25 and cycletick < STRIP_LENGTH * 2.25:
-        set_pixel(pixel_left, int(tick % segment_length), (0, 0, 0))
+        set_pixel(dual_strip, int(tick % segment_length), (0, 0, 0))
     elif cycletick > STRIP_LENGTH * 2.5 and cycletick < STRIP_LENGTH * 3.5:
-        set_pixel(pixel_left, int(STRIP_LENGTH - 1 - (tick % segment_length)), (255, 0, 255))
+        set_pixel(dual_strip, int(STRIP_LENGTH - 1 - (tick % segment_length)), (255, 0, 255))
     elif cycletick > STRIP_LENGTH * 3.75 and cycletick < STRIP_LENGTH * 4.75:
-        set_pixel(pixel_left, int(STRIP_LENGTH - 1 - (tick % segment_length)), (0, 0, 0))
+        set_pixel(dual_strip, int(STRIP_LENGTH - 1 - (tick % segment_length)), (0, 0, 0))
     elif cycletick == (STRIP_LENGTH * 5) - 1:
-        fill_strip(pixel_left, (0, 0, 0))
+        fill_strip(dual_strip, (0, 0, 0))
         # print("clear")
 
 def climbed():
@@ -121,7 +124,7 @@ def climbed():
     value = value + 50
     value = int(round(value))
     
-    fill_strip(pixel_left, (value, 0, value))
+    fill_strip(dual_strip, (value, 0, value))
     
 def alert():
     tick = int(round((0 - utime.ticks_ms() )/ 15))
@@ -129,11 +132,11 @@ def alert():
     value = tick % 25
     value = value * 10
     
-    fill_strip(pixel_left, (value, value * 0.7, value * 0.6))
+    fill_strip(dual_strip, (value, value * 0.7, value * 0.6))
           
           
-pixel_left.fill((0, 0, 0))
-pixel_left.write()
+dual_strip.fill((0, 0, 0))
+dual_strip.write()
 
 utime.sleep(1)
 
@@ -143,5 +146,4 @@ while True:
     shoot()
     
     utime.sleep_ms(5) #adjust for peak frames per second of the robot, 20ms is approx 50fps max rendering ability
-    pixel_left.write()
-
+    dual_strip.write()
